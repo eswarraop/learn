94055c91900b606f494f23c6188624f193f68370
Eswar <eswarraop@gmail.com>
2018-09-22 10:56:45 -0500

    working on mpsyt

--- aiy/mpsyt.py
+++ aiy/mpsyt.py
@@ -2,24 +2,50 @@
 
 
 import subprocess
+
 import os
+import code
+import time
 
 
-class mpsyt_runner( ):
+class player():
 
     def __init__(self):
+
+        log_file = "mpsyt.log"
+        self.log = open(log_file, 'w')
+        err_file = "mpsyt.err"
+        self.err = open(err_file, 'w')
+
         self.mpsyt = subprocess.Popen(["/usr/local/bin/mpsyt",""],stdin=subprocess.PIPE,
-                                                                  stdout=subprocess.PIPE,
+                                                                  stdout=self.log,
+                                                                  stderr=self.err,
                                                                   shell=True)
-        self.mpsyt.stdin.write(bytes('set playerargs --volume=30\n', 'utf-8'))
-        self.mpsyt.stdin.flush()
+
+        time.sleep(1)
+        self.send_command('set playerargs --volume=30')
 
     
     def send_command( self, line):
+        line = line + os.linesep
         self.mpsyt.stdin.write(bytes(line, 'utf-8'))
         self.mpsyt.stdin.flush()
 
         #code.interact( local  = locals() )
+
+    def play_song(self, line):
+        command = "/{0}".format( line ) 
+        self.send_command(command)
+        time.sleep(1)
+        self.send_command("1")
+
+    def play_list(self, line):
+        command = "//{0}".format( line )
+        self.send_command(command)
+        time.sleep(1)
+        self.send_command("1")
+        time.sleep(1)
+        self.send_command("1")
 
 
     def kill(self):
@@ -38,10 +64,21 @@ class mpsyt_runner( ):
         number = int(number)
         self.send_command("set playerargs --volume={0}".format( number ))
 
+    def increase_volume(self ):
+        self.send_command("9".format( number ))
+
+    def decrease_volume(self ):
+        self.send_command("0".format( number ))
+
+
 
 
 
 if __name__ == '__main__':
 
-    obj = mpsyt_runner()
+    obj = player()
+    obj.play_song("geeta govindam")
+
+    code.interact( local = locals() )
+
 
