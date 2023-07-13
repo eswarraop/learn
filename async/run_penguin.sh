6a371aa2e1069369a7889c4db737288472aea847
Eswar <eswarraop@gmail.com>
2023-07-12 23:02:29 -0500

    added separate config

--- async/run_penguin.sh
+++ async/run_penguin.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 
-
-sudo curl -X PUT --data-binary '{
-  "listeners": {
-      "*:80": {
-          "pass": "applications/spaceclock_asgi"
-      }
-  },
-  "applications": {
-      "spaceclock_asgi": {
-          "processes": 2,
-          "limits": {
-              "timeout": 3,
-              "requests": 500
-
-          },
-          "type": "python",
-          "path": "/home/eswar/learn/async/",
-          "module": "asgi"
-      }
-  }
-  }' --unix-socket /var/run/control.unit.sock http://10.0.0.20/config/
-
-sudo curl -X GET --unix-socket /var/run/unit/control.unit.sock \
-      http://localhost/control/applications/spaceclock_asgi/restart
+sudo curl -X GET --unix-socket /var/run/control.unit.sock http://10.0.0.20:8192/control/applications/spaceclock_asgi/restart
+sudo curl -X GET --unix-socket /var/run/control.unit.sock http://10.0.0.20:8192/config
+sudo curl -X PUT --data-binary @config.json --unix-socket /var/run/control.unit.sock http://10.0.0.20/config/
 
